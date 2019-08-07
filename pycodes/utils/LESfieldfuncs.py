762d12d1f34beafa39e8e44dcbb41203f3a1b66b
Jia He <jiahe.gatech@gmail.com>
2019-08-07 08:45:57 -0700

    compute the updraft average of scalar vertical gradient

--- pycodes/utils/LESfieldfuncs.py
+++ pycodes/utils/LESfieldfuncs.py
@@ -12,3 +12,13 @@ def get_updmask(dataobj, z_half):
     cb_idx, ct_idx = masking.calculate_cloud_base_top(ql_2d)
     mask2d = masking.updraft_env_mask(c_srf_2d, w2d_half, ql_2d, cb_idx, ct_idx, z_half)
     return mask2d['updraft'].reshape(c_srf.shape[0],c_srf.shape[1],c_srf.shape[2])
+
+def scalar_ddz_upd(dataobj, varname, mask3d, dz, alpha0):
+    var = dataobj.groups['fields'].variables[varname][:].data
+    ddz = np.zeros_like(var)
+    for k in np.arange(1, var.shape[2]-1):
+        ddz[:,:,k] = 0.5*(alpha0[k+1]*var[:,:,k+1]-alpha0[k-1]*var[:,:,k-1])/float(dz)
+    return np.ma.masked_where(mask3d, ddz).reshape(ddz.shape[0]*ddz.shape[1],ddz.shape[2]).mean(axis=0)
+
+def get_a_upd(mask3d):
+    return 1-mask3d.sum(axis=0).sum(axis=0)/float(mask3d.shape[0]*mask3d.shape[1])
